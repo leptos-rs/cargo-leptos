cd8c0c031f8bf02cb54a29acabdea87c7ab6c1d3
Michal Vavra <michal.vavra@gmail.com>
2022-11-30 16:38:48 +0100

    Fix `cargo-generate` arguments and binary URL (#20)
    * Update `cargo-generate` binary url
    
    * Fix `cargo-generate` command to have correct args

--- src/run/new.rs
+++ src/run/new.rs
@@ -54,6 +54,7 @@ impl NewCommand {
         let args = self.to_args();
         let exe = cargo_generate_exe()?;
         let mut process = Command::new(exe)
+            .arg("generate")
             .args(&args)
             .spawn()
             .context("Could not spawn command")?;
@@ -80,9 +81,11 @@ fn bool_push(args: &mut Vec<String>, name: &str, set: bool) {
         args.push(format!("--{name}"))
     }
 }
+
 fn opt_push(args: &mut Vec<String>, name: &str, arg: &Option<String>) {
     if let Some(arg) = arg {
-        args.push(format!("--{name} {arg}"));
+        args.push(format!("--{name}"));
+        args.push(arg.clone());
     }
 }
 
@@ -109,9 +112,9 @@ fn cargo_generate_exe() -> Result<PathBuf> {
         ("linux", "x86_64") => "x86_64-unknown-linux-gnu",
         _ => bail!("No cargo-generate tar binary found for {target_os} {target_arch}"),
     };
-    let url = format!("https://github.com/cargo-generate/cargo-generate/releases/download/{version}/cargo-generate-{version}-{target}.tar.gz");
+    let url = format!("https://github.com/cargo-generate/cargo-generate/releases/download/v{version}/cargo-generate-v{version}-{target}.tar.gz");
 
-    let name = format!("cargo-generate-{version}");
+    let name = format!("cargo-generate-v{version}");
 
     match INSTALL_CACHE.download(true, &name, &[], &url) {
         Ok(None) => bail!("Unable to download cargo-generate for {target_os} {target_arch}"),
