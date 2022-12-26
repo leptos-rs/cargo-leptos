42a6afa4937a838a56250e0676de665d97a9cfc5
hakesson <henrik@akesson.mobi>
2022-12-26 18:13:45 +0100

    More file listing

--- src/ext/tests.rs
+++ src/ext/tests.rs
@@ -10,15 +10,10 @@ async fn download_sass() {
     let dir = TempDir::new().unwrap();
     let meta = Exe::Sass.meta_with_dir(dir.path().to_path_buf()).unwrap();
     let e = meta.from_cache().await;
-    assert!(e.is_ok(), "{e:#?}\n{:#?}", meta);
+    assert!(e.is_ok(), "{e:#?}\n{:#?}\nFiles: \n {}", meta, ls(&dir));
 
     let e = e.unwrap();
-    assert!(
-        e.exists(),
-        "{:#?}\nFile listing: \n{}",
-        meta,
-        ls(&dir.path()).unwrap_or_default()
-    );
+    assert!(e.exists(), "{:#?}\nFiles: \n{}", meta, ls(&dir));
 }
 
 #[tokio::test]
@@ -29,15 +24,10 @@ async fn download_cargo_generate() {
         .unwrap();
 
     let e = meta.from_cache().await;
-    assert!(e.is_ok(), "{e:#?}\n{:#?}", meta);
+    assert!(e.is_ok(), "{e:#?}\n{:#?}\nFiles: \n {}", meta, ls(&dir));
 
     let e = e.unwrap();
-    assert!(
-        e.exists(),
-        "{:#?}\nFile listing: \n{}",
-        meta,
-        ls(&dir.path()).unwrap_or_default()
-    );
+    assert!(e.exists(), "{:#?}\nFiles: \n{}", meta, ls(&dir));
 }
 
 #[tokio::test]
@@ -47,18 +37,17 @@ async fn download_wasmopt() {
         .meta_with_dir(dir.path().to_path_buf())
         .unwrap();
     let e = meta.from_cache().await;
-    assert!(e.is_ok(), "{e:#?}\n{:#?}", meta);
+    assert!(e.is_ok(), "{e:#?}\n{:#?}\nFiles: \n {}", meta, ls(&dir));
 
     let e = e.unwrap();
-    assert!(
-        e.exists(),
-        "{:#?}\nFile listing: \n{}",
-        meta,
-        ls(&dir.path()).unwrap_or_default()
-    );
+    assert!(e.exists(), "{:#?}\nFiles: \n{}", meta, ls(&dir));
 }
 
-fn ls(root: &Path) -> Result<String> {
+fn ls(dir: &TempDir) -> String {
+    ls_path(&dir.path()).unwrap_or_default()
+}
+
+fn ls_path(root: &Path) -> Result<String> {
     let mut dirs: VecDeque<(usize, Utf8PathBuf)> = VecDeque::new();
     let root = Utf8PathBuf::from_path_buf(root.to_path_buf()).unwrap();
 
