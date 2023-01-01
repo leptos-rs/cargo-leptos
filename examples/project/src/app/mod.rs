30066df77682f272224142fc3991bfd2120f56a7
Ben Wishovich <ben@benw.is>
2023-01-01 11:18:14 -0800

    Update project example to use leptos integrations and for the latest 0.1.0 renderer

--- examples/project/src/app/mod.rs
+++ examples/project/src/app/mod.rs
@@ -2,12 +2,12 @@ use leptos::*;
 use leptos_meta::*;
 
 #[component]
-pub fn App(cx: Scope) -> Element {
-    provide_context(cx, MetaContext::default());
-
+pub fn App(cx: Scope) -> impl IntoView {
+    provide_meta_context(cx);
     view! {
         cx,
         <div>
+            <Stylesheet id="leptos" href="./target/site/pkg/example.css" />
             <Title text="Cargo Leptos" />
             <h1>"Hi from your Leptos WASM!"</h1>
         </div>
