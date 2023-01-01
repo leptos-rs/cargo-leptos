10465a3e62373dc2c2d8e07e4e1db0359968b152
Ben Wishovich <ben@benw.is>
2023-01-01 11:48:56 -0800

    Add StyleSheet tags for CSS to Workspace

--- examples/workspace/project2/src/app/mod.rs
+++ examples/workspace/project2/src/app/mod.rs
@@ -8,6 +8,7 @@ pub fn App(cx: Scope) -> impl IntoView {
     view! {
         cx,
         <div>
+            <Stylesheet id="leptos" href="./target/site/project2/pkg/project2.css"/>
             <Title text="Cargo Leptos" />
             <h1>"Hi from your Leptos WASM!"</h1>
         </div>
