c9437d820d7cb48d582a6287348ed84bbea5edaf
benwis <ben@celcyon.com>
2023-02-23 09:34:35 -0800

    Change test types

--- src/ext/tests.rs
+++ src/ext/tests.rs
@@ -6,7 +6,9 @@ use temp_dir::TempDir;
 #[tokio::test]
 async fn download_sass() {
     let dir = TempDir::new().unwrap();
-    let meta = Exe::Sass.meta_with_dir(dir.path().to_path_buf()).unwrap();
+    let meta = Exe::Sass
+        .meta_with_dir(Some(dir.path().to_path_buf()))
+        .unwrap();
     let e = meta.from_cache().await;
     assert!(e.is_ok(), "{e:#?}\n{:#?}\nFiles: \n {}", meta, ls(&dir));
 
@@ -18,7 +20,7 @@ async fn download_sass() {
 async fn download_cargo_generate() {
     let dir = TempDir::new().unwrap();
     let meta = Exe::CargoGenerate
-        .meta_with_dir(dir.path().to_path_buf())
+        .meta_with_dir(Some(dir.path().to_path_buf()))
         .unwrap();
 
     let e = meta.from_cache().await;
@@ -32,7 +34,7 @@ async fn download_cargo_generate() {
 async fn download_wasmopt() {
     let dir = TempDir::new().unwrap();
     let meta = Exe::WasmOpt
-        .meta_with_dir(dir.path().to_path_buf())
+        .meta_with_dir(Some(dir.path().to_path_buf()))
         .unwrap();
     let e = meta.from_cache().await;
     assert!(e.is_ok(), "{e:#?}\n{:#?}\nFiles: \n {}", meta, ls(&dir));
