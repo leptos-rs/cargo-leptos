12927b825ff5aa8217c787b07c44c2ae0467a528
hakesson <henrik@akesson.mobi>
2023-03-06 10:06:53 +0100

    No parallel builds of examples

--- src/tests.rs
+++ src/tests.rs
@@ -2,7 +2,7 @@ use camino::Utf8PathBuf;
 
 use crate::{
     config::{Cli, Commands, Opts},
-    ext::{fs, PathBufExt},
+    ext::PathBufExt,
     run,
 };
 
@@ -16,12 +16,6 @@ async fn workspace_build() {
         command,
     };
 
-    if Utf8PathBuf::from("examples/workspace/target").exists() {
-        fs::rm_dir_content("examples/workspace/target")
-            .await
-            .unwrap();
-    }
-
     run(cli).await.unwrap();
 
     // when running the current working directory is changed to the manifest path.
@@ -29,3 +23,24 @@ async fn workspace_build() {
 
     insta::assert_display_snapshot!(site_dir.ls_ascii(0).unwrap_or_default());
 }
+
+// TODO: `cargo-leptos` sets the cwd which is a global env
+// and that prevents builds to run in parallel in the same process
+//
+// #[tokio::test]
+// async fn project_build() {
+//     let command = Commands::Build(Opts::default());
+
+//     let cli = Cli {
+//         manifest_path: Some(Utf8PathBuf::from("examples/project/Cargo.toml")),
+//         log: Vec::new(),
+//         command,
+//     };
+
+//     run(cli).await.unwrap();
+
+//     // when running the current working directory is changed to the manifest path.
+//     let site_dir = Utf8PathBuf::from("target/site");
+
+//     insta::assert_display_snapshot!(site_dir.ls_ascii(0).unwrap_or_default());
+// }
