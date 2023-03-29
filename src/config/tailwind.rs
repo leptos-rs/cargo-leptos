94cbd2018be9ad542a53242b834b47ecdf592a41
Emil Gardström <emil.gardstrom@gmail.com>
2023-03-29 21:29:32 +0200

    use relative path to config file for tailwind

--- src/config/tailwind.rs
+++ src/config/tailwind.rs
@@ -11,17 +11,21 @@ pub struct TailwindConfig {
 
 impl TailwindConfig {
     pub fn new(conf: &ProjectConfig) -> Result<Option<Self>> {
-        let Some(input_file) = conf.tailwind_input_file.clone() else {
+        let input_file = if let Some(input_file) = conf.tailwind_input_file.clone() {
+            conf.config_dir.join(input_file)
+        } else {
             if conf.tailwind_config_file.is_some() {
                 bail!("The Cargo.toml `tailwind-input-file` is required when using `tailwind-config-file`]");
             }
             return Ok(None);
         };
 
-        let config_file = conf
-            .tailwind_config_file
-            .clone()
-            .unwrap_or_else(|| Utf8PathBuf::from("./tailwind.config.js"));
+        let config_file = conf.config_dir.join(
+            conf.tailwind_config_file
+                .clone()
+                .unwrap_or_else(|| Utf8PathBuf::from("./tailwind.config.js")),
+        );
+
         Ok(Some(Self {
             input_file,
             config_file,
