ad604d2163883bc0ba0e20a9e1d1fed26750f545
Mark Catley <mark@catley.net.nz>
2023-04-13 12:44:28 +1200

    fix: cause error when a build or test fails
    fixes #114

--- src/command/test.rs
+++ src/command/test.rs
@@ -1,26 +1,36 @@
 use crate::compile::{front_cargo_process, server_cargo_process};
 use crate::config::{Config, Project};
-use crate::ext::anyhow::{Context, Result};
+use crate::ext::anyhow::{Context, Result, anyhow};
 use crate::logger::GRAY;
 
 pub async fn test_all(conf: &Config) -> Result<()> {
+    let mut first_failed_project = None;
+
     for proj in &conf.projects {
-        test_proj(proj).await?;
+        if !test_proj(proj).await? && first_failed_project.is_none() {
+            first_failed_project = Some(proj);
+        }
     }
-    Ok(())
+
+    if let Some(proj) = first_failed_project {
+        Err(anyhow!("Tests failed for {}", proj.name))
+    } else {
+        Ok(())
+    }
 }
 
-pub async fn test_proj(proj: &Project) -> Result<()> {
+pub async fn test_proj(proj: &Project) -> Result<bool> {
     let (envs, line, mut proc) = server_cargo_process("test", proj).dot()?;
 
-    proc.wait().await.dot()?;
+    let server_exit_status = proc.wait().await.dot()?;
     log::debug!("Cargo envs: {}", GRAY.paint(envs));
     log::info!("Cargo server tests finished {}", GRAY.paint(line));
 
     let (envs, line, mut proc) = front_cargo_process("test", false, proj).dot()?;
 
-    proc.wait().await.dot()?;
+    let front_exit_status = proc.wait().await.dot()?;
     log::debug!("Cargo envs: {}", GRAY.paint(envs));
     log::info!("Cargo front tests finished {}", GRAY.paint(line));
-    Ok(())
+
+    Ok(server_exit_status.success() && front_exit_status.success())
 }
