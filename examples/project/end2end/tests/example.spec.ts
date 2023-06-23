7525fccfbc816955b9342bb1716848a4238bee77
kruserr <46799551+kruserr@users.noreply.github.com>
2023-06-23 16:29:32 +0000

    test(example): update test

--- examples/project/end2end/tests/example.spec.ts
+++ examples/project/end2end/tests/example.spec.ts
@@ -5,5 +5,5 @@ test("homepage has title and links to intro page", async ({ page }) => {
 
   await expect(page).toHaveTitle("Cargo Leptos");
 
-  await expect(page.locator("h1")).toHaveText("Hi from your Leptos WASM!");
+  await expect(page.locator("h2")).toHaveText("Welcome to Leptos");
 });
