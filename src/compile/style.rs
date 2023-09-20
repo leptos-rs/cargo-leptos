010b65fd091769233fe2bb8ec068b05d6165b8d8
Paulo Coelho <9609090+prscoelho@users.noreply.github.com>
2023-09-20 02:31:08 +0100

    Specify browser targets in minify step (#194)

--- src/compile/style.rs
+++ src/compile/style.rs
@@ -91,16 +91,21 @@ fn browser_lists(query: &str) -> Result<Option<Browsers>> {
 
 async fn process_css(proj: &Project, css: String) -> Result<Product> {
     let browsers = browser_lists(&proj.style.browserquery).context("leptos.style.browserquery")?;
+    let targets = Targets::from(browsers);
 
     let mut stylesheet =
         StyleSheet::parse(&css, ParserOptions::default()).map_err(|e| anyhow!("{e}"))?;
 
     if proj.release {
-        stylesheet.minify(MinifyOptions::default())?;
+        let minify_options = MinifyOptions {
+            targets,
+            ..Default::default()
+        };
+        stylesheet.minify(minify_options)?;
     }
 
     let options = PrinterOptions::<'_> {
-        targets: Targets::from(browsers),
+        targets,
         minify: proj.release,
         ..Default::default()
     };
