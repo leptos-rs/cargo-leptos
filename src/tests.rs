f983c292b32bd77c5369fd12708d461804b2c9ec
benwis <ben@celcyon.com>
2023-11-19 23:06:50 -0800

    No snap

--- src/tests.rs
+++ src/tests.rs
@@ -21,7 +21,7 @@ async fn workspace_build() {
     // when running the current working directory is changed to the manifest path.
     let site_dir = Utf8PathBuf::from("target/site");
 
-    insta::assert_display_snapshot!(site_dir.ls_ascii(0).unwrap_or_default());
+    //insta::assert_display_snapshot!(site_dir.ls_ascii(0).unwrap_or_default());
 }
 
 // TODO: `cargo-leptos` sets the cwd which is a global env
