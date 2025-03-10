5c3a64f2cf835662072734090111125516155ded
SleeplessOne1917 <28871516+SleeplessOne1917@users.noreply.github.com>
2025-03-09 22:36:29 -0400

    Update version of example to 0.7.x

--- examples/project/src/server.rs
+++ examples/project/src/server.rs
@@ -1,32 +1,50 @@
 use crate::app::*;
 use actix_files::Files;
-use actix_web::*;
-use leptos::*;
+use actix_web::{App as ActixApp, *};
+use leptos::{config::get_configuration, logging::warn, prelude::*};
 use leptos_actix::{generate_route_list, LeptosRoutes};
+use leptos_meta::MetaTags;
 
 pub async fn run() -> std::io::Result<()> {
     _ = dotenvy::dotenv();
 
-    let conf = get_configuration(None).await.unwrap();
+    let conf = get_configuration(None).unwrap();
     let addr = conf.leptos_options.site_addr;
 
     warn!("serving at {addr}");
 
-    // Generate the list of routes in your Leptos App
-    let routes = generate_route_list(|| view! { <App /> });
-
     HttpServer::new(move || {
+        // Generate the list of routes in your Leptos App
+        let routes = generate_route_list(App);
         let leptos_options = &conf.leptos_options;
+        let site_root = &leptos_options.site_root;
 
-        let site_root = leptos_options.site_root.clone();
+        ActixApp::new()
+            .leptos_routes(routes, {
+                let options = leptos_options.clone();
 
-        App::new()
-            .leptos_routes(
-                leptos_options.to_owned(),
-                routes.to_owned(),
-                || view! { <App /> },
-            )
-            .service(Files::new("/", site_root.to_owned()))
+                move || {
+                    view! {
+                        <!DOCTYPE html>
+                        <html lang="en">
+                            <head>
+                                <meta charset="utf-8" />
+                                <meta
+                                    name="viewport"
+                                    content="width=device-width, initial-scale=1"
+                                />
+                                <AutoReload options=options.clone() />
+                                <HydrationScripts options=options.clone() />
+                                <MetaTags />
+                            </head>
+                            <body>
+                                <App />
+                            </body>
+                        </html>
+                    }
+                }
+            })
+            .service(Files::new("/", site_root.as_ref()))
             .wrap(middleware::Compress::default())
     })
     .bind(&addr)?
