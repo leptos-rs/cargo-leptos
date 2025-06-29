80b99c002b6084ce7fffd19193bdddca2ae993a7
Mahdi <mtzarrin@gmail.com>
2025-06-29 10:39:25 +0330

    feat: add environment variable logging before executed commands

--- src/compile/mod.rs
+++ src/compile/mod.rs
@@ -22,15 +22,17 @@ use tokio::process::Command;
 
 fn build_cargo_command_string(command: &Command) -> String {
   let std_command = command.as_std();
-  let program = std_command.get_program();
-  let args = std_command.get_args();
-
-  [program]
-    .into_iter()
-    .chain(args)
-    .map(|arg| match arg.to_string_lossy() {
-      arg if arg.contains(' ') => format!("'{arg}'"),
-      arg => arg.into_owned(),
+  let envs = std_command.get_envs().into_iter().filter_map(|(name, val)| {
+    val.map(|val| {
+      let name = name.to_string_lossy();
+      let val = val.to_string_lossy();
+      format!("{name}={val}")
     })
-    .join(" ")
+  });
+  let program = std_command.get_program().to_string_lossy().into_owned();
+  let args = std_command.get_args().map(|arg| match arg.to_string_lossy() {
+    arg if arg.contains(' ') => format!("'{arg}'"),
+    arg => arg.into_owned(),
+  });
+  envs.chain([program]).chain(args).join(" ")
 }
