2d1cd72678aad04860e7585bdce3b0134d9be748
Greg Johnston <greg.johnston@gmail.com>
2025-08-26 21:27:46 -0400

    add additional uniqueness

--- src/wasm_split_tools/split_point.rs
+++ src/wasm_split_tools/split_point.rs
@@ -5,6 +5,8 @@ use super::read::{ExportId, ImportId, InputFuncId, InputModule, SymbolIndex};
 use crate::config::Project;
 use crate::internal_prelude::*;
 use regex::Regex;
+use std::fmt::Write;
+use std::hash::{DefaultHasher, Hash, Hasher};
 use std::sync::LazyLock;
 
 #[derive(Debug, PartialEq, Eq, Clone)]
@@ -256,9 +258,15 @@ impl SplitModuleIdentifier {
             Self::Split { name, .. } => name.clone(),
             Self::Chunk { splits, hash } => {
                 let mut full_name = splits.join("_");
-                if full_name.len() > 128 {
-                    full_name.truncate(128);
+                if full_name.len() > 200 {
+                    full_name.truncate(200);
                     full_name.push_str(hash);
+
+                    let mut hasher = DefaultHasher::new();
+                    splits.hash(&mut hasher);
+                    let hash = hasher.finish();
+                    full_name.push('_');
+                    write!(&mut full_name, "{:?}", hash).unwrap();
                 }
                 full_name
             }
@@ -271,8 +279,8 @@ impl SplitModuleIdentifier {
             Self::Split { name, hash } => format!("{name}.{hash}"),
             Self::Chunk { splits, hash } => {
                 let mut splits = splits.join("_");
-                if splits.len() > 128 {
-                    splits.truncate(128);
+                if splits.len() > 200 {
+                    splits.truncate(200);
                 }
                 format!("{splits}.{hash}")
             }
